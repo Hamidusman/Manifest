b99a32ae23d24a72bc8b90c7d6c8cfae92567426
Hamidusman <abdulhamidusman218@gmail.com>
2023-12-25 12:57:45 +0000

    major setup

--- core/views.py
+++ core/views.py
@@ -1,7 +1,40 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import get_user_model
 # Create your views here.
 
-
+User = get_user_model()
 def index(request):
     return render(request, 'index.html')
+
+
+def register(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+
+        if password1 == password2:
+            if User.objects.filter(username=username).exists():
+                message.info(request, 'username already taken')
+                return redirect('register')
+
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already in Exists')
+                return redirect('register')
+
+            else: 
+                user = User.objects.create_user(username=username, email=email, password=password1)
+                user.save()
+                messages.success(request, 'Registration successful. You can now login.')
+                return redirect('main')
+        else:
+            messages.info('Password dont match')
+            return redirect('register')
+    return render(request, 'register.html')
+
+def main(request):
+    return render(request, 'main.html')
